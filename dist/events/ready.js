024ece5ae58d0b1827628ecbe14b186a0fea0976
Christy Campbell <christytc10@gmail.com>
2023-09-24 20:53:35 +0100

    wip

--- dist/events/ready.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Event_1 = require("../structures/Event");
-exports.default = new Event_1.Event("ready", (client) => {
-    console.log(`${client.user?.tag} is now online!`);
-});
